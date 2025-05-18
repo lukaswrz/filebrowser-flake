07f5c8607591364308cd785047268b1f26ee2e9e
Lukas Wurzinger <lukas@wrz.one>
2025-05-18 22:07:00 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "FileBrowser";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    {
+      self,
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.nixosModules.default = import ./module.nix;
+    };
+}
